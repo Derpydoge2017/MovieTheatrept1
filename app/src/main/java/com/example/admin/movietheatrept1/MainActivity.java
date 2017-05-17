74c230e597c441cd0728ba676b9cfcc900fb4cae
Tony <thanhlong.le2002@gmail.com>
2017-05-17 18:51:37 +0700

    Changed some stuff Added networking parts

--- app/src/main/java/com/example/admin/movietheatrept1/MainActivity.java
+++ app/src/main/java/com/example/admin/movietheatrept1/MainActivity.java
@@ -4,9 +4,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.example.admin.movietheatrept1.utils.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mFilmTextView;
+
+    private TextView mUrlDisplayData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,46 +25,68 @@ public class MainActivity extends AppCompatActivity {
          */
         mFilmTextView = (TextView) findViewById(R.id.tv_film_data);
 
+        mUrlDisplayData = (TextView) findViewById(R.id.tv_url_display);
+
         /*
          * This String array contains dummy film data. Later in the course, we're going to get
          * real weather data. For now, we want to get something on the screen as quickly as
          * possible, so we'll display this dummy data.
          */
-        String[] dummyFilmData = {
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-                "Iron Man, May 17",
-        };
+        //String[] dummyFilmData = {
+             //   "Iron Man, May 17",
+             //  "Iron Man, May 17",
+             //   "Iron Man, May 17",
+             //   "Iron Man, May 17",
+             //   "Iron Man, May 17",
+             //   "Iron Man, May 17",
+            //    "Iron Man, May 17",
+            //    "Iron Man, May 17",
+            //    "Iron Man, May 17",
+            //    "Iron Man, May 17",
+           //     "Iron Man, May 17",
+          //      "Iron Man, May 17",
+         //       "Iron Man, May 17",
+         //       "Iron Man, May 17",
+        //        "Iron Man, May 17",
+        //        "Iron Man, May 17",
+        //        "Iron Man, May 17",
+        //        "Iron Man, May 17",
+        //        "Iron Man, May 17",
+        //        "Iron Man, May 17",
+        //        "Iron Man, May 17",
+        //        "Iron Man, May 17",
+        //        "Iron Man, May 17",
+        //        "Iron Man, May 17",
+        //        "Iron Man, May 17",
+        //};
+
+        makeMovieSearchQuery();
+
+
 
         /*
          * Iterate through the array and append the Strings to the TextView. The reason why we add
          * the "\n\n\n" after the String is to give visual separation between each String in the
          * TextView. Later, we'll learn about a better way to display lists of data.
          */
-        for (String dummyFilmDay : dummyFilmData) {
-            mFilmTextView.append(dummyFilmDay + "\n\n");
+        //for (String dummyFilmDay : dummyFilmData) {
+        //    mFilmTextView.append(dummyFilmDay + "\n\n");
+        //}
+
+
+
+    }
+
+    private void makeMovieSearchQuery() {
+        URL movieSearchUrl = NetworkUtils.buildUrl();
+        mUrlDisplayData.setText(movieSearchUrl.toString());
+        String movieSearchResults = null;
+        try {
+            movieSearchResults = NetworkUtils.getResponseFromHttpUrl(movieSearchUrl);
+            mUrlDisplayData.setText(movieSearchResults);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
 }
